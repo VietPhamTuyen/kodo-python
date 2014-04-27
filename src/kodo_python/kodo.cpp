9be3d8dcd21770930f4ad5bb8409f21a698ea194
Jeppe Pihl <jpihl08@gmail.com>
2014-04-27 15:01:06 +0200

    more work

--- src/kodo_python/kodo.cpp
+++ src/kodo_python/kodo.cpp
@@ -3,11 +3,53 @@
 #include <fifi/default_field.hpp>
 #include <kodo/rlnc/full_vector_codes.hpp>
 
-#include "encoder.hpp"
-
-
+#include <sak/storage.hpp>
+#include <kodo/systematic_operations.hpp>
 namespace kodo_python
 {
+    template<class T>
+    boost::python::list std_vector_to_py_list(const std::vector<T>& v)
+    {
+        boost::python::object get_iter = boost::python::iterator<std::vector<T> >();
+        boost::python::object iter = get_iter(v);
+        boost::python::list l(iter);
+        return l;
+    }
+
+
+    bool is_systematic(const kodo::full_rlnc_encoder<fifi::binary>& encoder)
+    {
+        return kodo::is_systematic_encoder(encoder);
+    }
+
+    bool is_systematic_on(const kodo::full_rlnc_encoder<fifi::binary>& encoder)
+    {
+        return kodo::is_systematic_on(encoder);
+    }
+
+    void set_systematic_on(kodo::full_rlnc_encoder<fifi::binary>& encoder)
+    {
+        kodo::set_systematic_on(encoder);
+    }
+
+    void set_systematic_off(kodo::full_rlnc_encoder<fifi::binary>& encoder)
+    {
+        kodo::set_systematic_off(encoder);
+    }
+
+    void set_symbols(kodo::full_rlnc_encoder<fifi::binary>& encoder, std::string data)
+    {
+        auto storage = sak::const_storage((uint8_t*)data.c_str(), data.length());
+        encoder.set_symbols(storage);
+    }
+
+    boost::python::list encode(kodo::full_rlnc_encoder<fifi::binary>& encoder)
+    {
+        std::vector<int> payload(encoder.payload_size());
+        encoder.encode((uint8_t*)payload.data());
+        return std_vector_to_py_list(payload);
+    }
+
     BOOST_PYTHON_MODULE(kodo)
     {
         typedef kodo::full_rlnc_encoder<fifi::binary>::factory factory_type;
@@ -22,7 +64,7 @@ namespace kodo_python
             .def("max_payload_size", &factory_type::max_payload_size)
         ;
 
-        typedef encoder<kodo::full_rlnc_encoder<fifi::binary>> encoder_type;
+        typedef kodo::full_rlnc_encoder<fifi::binary> encoder_type;
         boost::python::class_<encoder_type, boost::noncopyable>("encoder",
             boost::python::no_init)
             .def("payload_size", &encoder_type::payload_size)
@@ -31,13 +73,13 @@ namespace kodo_python
             .def("symbols", &encoder_type::symbols)
             .def("rank", &encoder_type::rank)
             .def("is_symbol_pivot", &encoder_type::is_symbol_pivot)
-            .def("encode", &encoder_type::encode)
-            .def("set_symbols", &encoder_type::set_symbols)
+            .def("encode", encode)
+            .def("set_symbols", set_symbols)
             .def("set_symbol", &encoder_type::set_symbol)
-            .def("is_systematic", &encoder_type::is_systematic)
-            .def("is_systematic_on", &encoder_type::is_systematic_on)
-            .def("set_systematic_on", &encoder_type::set_systematic_on)
-            .def("set_systematic_off", &encoder_type::set_systematic_off)
+            .def("is_systematic", is_systematic)
+            .def("is_systematic_on", is_systematic_on)
+            .def("set_systematic_on", set_systematic_on)
+            .def("set_systematic_off", set_systematic_off)
         ;
 
         boost::python::register_ptr_to_python<boost::shared_ptr<encoder_type>>();
