84d08e888a039dbb8addd95d2e204d75aa749f0e
Jeppe Pihl <jpihl08@gmail.com>
2014-12-04 10:29:58 +0100

    combine lena and decoder state example

--- examples/lena.py
+++ examples/lena.py
@@ -20,6 +20,7 @@ import time
 try:
     import pygame
     import pygame.locals
+    import pygame.gfxdraw
 except:
     import sys
     print("Unable to import pygame module, please make sure it is installed.")
@@ -28,25 +29,13 @@ except:
 import numpy
 
 
-class ImageViewer(object):
-    """
-    A class containing the logic for displaying an image during decoding
-    """
+class CanvasEngine(object):
     def __init__(self, width, height):
-        """
-        Construct ImageViewer object
-
-        :param width: the width of the picture to be displayed
-        :param height: the height of the picture to be displayed
-        """
-        super(ImageViewer, self).__init__()
         self.lock = threading.Lock()
         self.screen = None
         self.running = False
         self.size = (width, height)
-        self.data_size = width * height * 3
-        self.thread = threading.Thread(
-            name='image_viewer', target=self.__start)
+        self.thread = threading.Thread(name='canvas', target=self.__start)
 
     def start(self):
         """Start a thread which runs the viewer logic"""
@@ -59,7 +48,7 @@ class ImageViewer(object):
         with self.lock:
             self.running = True
             pygame.init()
-            pygame.display.set_caption('Kodo-python ImageViewer')
+            pygame.display.set_caption('Example Canvas')
             self.screen = pygame.display.set_mode(self.size, pygame.NOFRAME)
             self.screen.fill((250, 250, 250))
 
@@ -73,6 +62,28 @@ class ImageViewer(object):
         self.running = False
         self.thread.join()
 
+    def add_surface(self, surface, position):
+        with self.lock:
+            self.screen.blit(surface, position)
+
+
+class ImageViewer(object):
+    """
+    A class containing the logic for displaying an image during decoding
+    """
+    def __init__(self, width, height, canvas, canvas_position=(0, 0)):
+        """
+        Construct ImageViewer object
+
+        :param width: the width of the picture to be displayed
+        :param height: the height of the picture to be displayed
+        """
+        super(ImageViewer, self).__init__()
+        self.size = (width, height)
+        self.data_size = width * height * 3
+        self.canvas = canvas
+        self.canvas_position = canvas_position
+
     def set_image(self, image_string):
         """
         Displays the provided string as an image.
@@ -81,56 +92,110 @@ class ImageViewer(object):
         and it should be equal to or larger than width * height * 3.
         """
         image_array = numpy.fromstring(
-            image_string[:self.data_size],
-            dtype=numpy.uint8)
+            image_string[:self.data_size], dtype=numpy.uint8)
+
         image_array.shape = self.size + (3,)
 
         # Show picture from top to buttom
         image_array = numpy.rot90(image_array)
+        image_surface = pygame.Surface(self.size)
+        pygame.surfarray.blit_array(image_surface, image_array)
+        self.canvas.add_surface(
+            image_surface,
+            self.canvas_position)
 
-        with self.lock:
-            pygame.surfarray.blit_array(self.screen, image_array)
 
+class DecodeStateViewer(object):
+    """Class for displaying the decoding coefficients"""
+    def __init__(self, symbols, size, canvas, canvas_position):
+        super(DecodeStateViewer, self).__init__()
+        self.surface = pygame.Surface((size, size))
+        self.padding = 10.0
+        self.diameter = (size - self.padding * 2.0) / symbols
+        self.canvas = canvas
+        self.canvas_position = canvas_position
 
-class ForceSystematicEncoder(object):
-    """Wrapper class to force an encoder to stay in systematic mode"""
-    def __init__(self, encoder):
-        self.encoder = encoder
-        self.packets = []
-        self.packet_count = 0
+    def trace_callback(self, zone, message):
+        """Callback to be used with the decoder trace API"""
+        # We are only interested in the decoder state.
+        if zone != "decoder_state":
+            return
 
-    def encode(self):
-        self.packet_count += 1
-        if self.packet_count != self.encoder.rank():
-            self.packets.append(self.encoder.encode())
+        decode_state = []
+        for line in message.split('\n'):
+            if not line:
+                continue
+            line = line.split()
 
-        return self.packets[(self.packet_count-1) % self.encoder.rank()-1]
+            decode_state.append({
+                'state': line[1][0],
+                'data': [int(i) for i in line[2:]]
+            })
 
-    def set_symbols(self, symbols):
-        return self.encoder.set_symbols(symbols)
+        self.show_decode_state(decode_state)
+
+    def show_decode_state(self, decode_state):
+        """
+        Use the decoding state to print a graphical representation.
+
+        :param decode_state: A list of dictionaries containing the symbol
+                             coefficients.
+        """
+
+        self.surface.fill((0,)*3)
+        y = self.padding + self.diameter / 2
+        for symbol in decode_state:
+            x = self.padding + self.diameter / 2
+            for data in symbol['data']:
+                x += self.diameter
+                if data == 0:
+                    continue
+                color = (255,)*3
+                if data != 1:
+                    color = (data % 255,) * 3
+                pygame.gfxdraw.circle(
+                    self.surface,
+                    int(x - self.diameter),
+                    int(y),
+                    int(self.diameter / 2),
+                    color)
+            y += self.diameter
+
+        self.canvas.add_surface(
+            self.surface,
+            self.canvas_position)
 
 
 def main():
-
     # Get directory of this file
     directory = os.path.dirname(os.path.realpath(__file__))
 
     # Load the image
     image = pygame.image.load(os.path.join(directory, 'lena.jpg'))
 
-    # Create an image viewer (the width and height of the image is needed).
-    image_viewer = ImageViewer(image.get_width(), image.get_height())
+    # Create example canvas.
+    canvas = CanvasEngine(image.get_width()*2, image.get_height())
+
+    # Create an image viewer
+    image_viewer = ImageViewer(image.get_width(), image.get_height(), canvas)
 
     # Convert the image into a string representation of a 3d (numpy) array
     # Rotate the array 270 degrees so that it's shown top down.
     data_in = numpy.rot90(pygame.surfarray.array3d(image), 3).tostring()
 
     # Pick a symbol size
-    symbol_size = 64
+    symbol_size = image.get_width()*3
 
     # Based on the size of the image and the symbol size, calculate the number
     # of symbols needed for containing this image in a single generation.
     symbols = int(math.ceil(float(len(data_in)) / symbol_size))
+
+    # Create an coefficient viewer
+    decode_state_viewer = DecodeStateViewer(
+        symbols=symbols,
+        size=max(image.get_width(), image.get_height()),
+        canvas=canvas,
+        canvas_position=(image.get_width(), 0))
 
     # Create encoder
     encoder_factory = kodo.FullVectorEncoderFactoryBinary(
@@ -140,11 +205,16 @@ def main():
     encoder = encoder_factory.build()
 
     # Create decoder
-    decoder_factory = kodo.FullVectorDecoderFactoryBinary(
+    decoder_factory = kodo.FullVectorDecoderFactoryBinaryTrace(
         max_symbols=symbols,
         max_symbol_size=symbol_size)
 
     decoder = decoder_factory.build()
+
+    # Set up tracing
+    if 'trace' in dir(decoder):
+        cb = lambda zone, msg: decode_state_viewer.trace_callback(zone, msg)
+        decoder.trace(cb)
 
     # Set the converted image data
     encoder.set_symbols(data_in)
@@ -152,7 +222,7 @@ def main():
     # Create an image viwer and run the following code in a try catch;
     # this prevents the program from locking up, as the finally clause will
     # close down the image viewer.
-    image_viewer.start()
+    canvas.start()
     try:
         packets = 0
         while not decoder.is_complete():
@@ -164,13 +234,13 @@ def main():
                 decoder.decode(packet)
 
             # limit the number of times we write to the screen (it's expensive)
-            if packets % (symbols // 5) == 0 or decoder.is_complete():
+            if packets % (symbols // 20) == 0 or decoder.is_complete():
                 image_viewer.set_image(decoder.copy_symbols())
 
         # Let the user see the photo before closing the application
         time.sleep(1)
     finally:
-        image_viewer.stop()
+        canvas.stop()
 
 
 if __name__ == "__main__":
