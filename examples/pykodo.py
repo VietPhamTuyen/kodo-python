4d2e50781530e268fe8ae0cdc8d9aef0b66a8940
Jeppe Pihl <jpihl08@gmail.com>
2014-12-16 10:38:14 +0100

    fixed pykodo factory for no_code

--- examples/pykodo.py
+++ examples/pykodo.py
@@ -47,6 +47,20 @@ def nested_add(dictionary, keys, value):
             current_dict = current_dict[key]
 
 
+def nested_get(dictionary, keys):
+    current_dict = dictionary
+
+    for key in keys:
+        if key in current_dict:
+            value = current_dict[key]
+            if type(value) == dict:
+                current_dict = value
+            else:
+                return value
+        else:
+            raise KeyError("{} not found.".format(" ".join(keys)))
+
+
 def split_upper_case(s):
     return [a for a in re.split(r'([A-Z][a-z]*\d*)', s) if a]
 
@@ -111,15 +125,20 @@ globals()['trace'] = True
 globals()['no_trace'] = False
 
 
-def decoder_factory(algorithm, field, max_symbols, max_symbol_size,
-                    trace=False):
+def __create_factory(algorithm, coder_type, trace, max_symbols,
+                     max_symbol_size, field=None):
+    location = [algorithm]
+    if field is not None:
+        location += [field]
     trace = 'trace' if trace else 'no_trace'
-    return __kodo_stacks[algorithm][field]['decoder'][trace](
-        max_symbols, max_symbol_size)
+    location += [coder_type, trace]
+
+    return nested_get(__kodo_stacks, location)(max_symbols, max_symbol_size)
 
 
-def encoder_factory(algorithm, field, max_symbols, max_symbol_size,
-                    trace=False):
-    trace = 'trace' if trace else 'no_trace'
-    return __kodo_stacks[algorithm][field]['encoder'][trace](
-        max_symbols, max_symbol_size)
+def decoder_factory(trace=False, **kwargs):
+    return __create_factory(coder_type="decoder", trace=trace, **kwargs)
+
+
+def encoder_factory(trace=False, **kwargs):
+    return __create_factory(coder_type="encoder", trace=trace, **kwargs)
